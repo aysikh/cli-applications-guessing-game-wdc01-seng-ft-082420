31f82afab3967b10a5fe4e159cdbb744348f078d
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-23 14:01:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,11 +7,11 @@ def random_number_generator
 end
 
 def user_input
-  gets.chomp()
+  gets.chomp().to_s
 end
 
 def results
-input.to_s = user_input
+input = user_input
 random_number = random_number_generator
   if input != random_number
     puts "Sorry! The computer guessed #{random_number}."
