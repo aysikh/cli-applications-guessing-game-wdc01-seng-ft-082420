7c45c54ff5c6c57b6aa2ccb632b5b661a9574fad
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-23 14:03:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,17 +2,17 @@ def user_prompt
   puts "Guess a number between 1-6 or type 'exit' to quit the game!"
 end
 
-def random_number_generator
+def random_number_generator #this is an integer
   rand(6)+1
 end
 
-def user_input
+def user_input #this is a string 
   gets.chomp()
 end
 
 def results
 input = user_input
-random_number = random_number_generator.to_s
+random_number = random_number_generator.to_s #we have to convert the integer to a string
   if input != random_number
     puts "Sorry! The computer guessed #{random_number}."
   elsif input == random_number
